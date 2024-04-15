2a0d9622e42f02184a417211c82171fe94764c8d
Asad Mughal <153642328+codingwithasad@users.noreply.github.com>
2024-04-15 18:04:51 +0500

    1st

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+let tl = gsap.timeline();
+tl.from('.first',{
+    y:400,
+    duration:3
+});
+tl.from('.second',{
+    y:500,
+    duration:3
+});
+tl.from('.third',{
+    y:600,
+    duration:3
+});
